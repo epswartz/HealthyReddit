a40da38e8768d7802c97c9b6a20ea7890f7ccd4e
epswartz <eswartzen@gmail.com>
2020-05-25 14:02:15 -0400

    minimal page working?

--- index.js
+++ index.js
@@ -5,16 +5,43 @@ function httpGet(theUrl) {
     return xmlHttp.responseText;
 }
 
-function updateDisplay(disp) {
-    document.getElementById("display").textContent = disp;
-}
-
 const now = new Date()
 const epoch = Math.round(now.getTime() / 1000)
 const oneDayAgo = epoch - 86400
 
-resp = httpGet("https://api.pushshift.io/reddit/search/submission?size=10&after=" + oneDayAgo.toString())
+resp = httpGet("https://api.pushshift.io/reddit/search/submission?size=10&subreddit=pics&after=" + oneDayAgo.toString())
 redditResp = JSON.parse(resp)
 const ids = redditResp.data.map(x => x.id)
-console.log(ids)
-updateDisplay(ids.join(", "))
+
+// Put the cards for posts into the column
+var column = document.currentScript.ownerDocument.querySelector("#post-column")
+var card_template = document.currentScript.ownerDocument.querySelector("#post-card")
+
+// Build and insert all the cards
+redditResp.data.forEach(e => {
+    console.log("Creating card for:", e.id)
+    var newCard = document.createElement("div")
+    newCard.className = "card bg-secondary mt-2"
+
+    var cardCol = document.createElement("div")
+    cardCol.className = "col mt-2"
+
+    var cardBlock = document.createElement("div")
+    cardBlock.className = "card-block"
+
+    var cardTitle = document.createElement("h4")
+    cardTitle.className = "card-title"
+    cardTitle.textContent = e.title
+
+    var linkButton = document.createElement("a")
+    linkButton.className = "btn btn-primary mr-2 mb-2"
+    linkButton.href = e.full_link
+    linkButton.role = "button"
+    linkButton.textContent = "Reddit Comments"
+
+    cardBlock.appendChild(cardTitle)
+    cardBlock.appendChild(linkButton)
+    cardCol.appendChild(cardBlock)
+    newCard.appendChild(cardCol)
+    column.appendChild(newCard)
+});
